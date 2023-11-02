5010f633351db63a9f6634198a23b100eb7c3d24
imagine2509 <imagine2509@gmail.com>
2023-11-02 21:14:21 +0300

    merged

--- client/src/components/Header/NavMenu.tsx
+++ client/src/components/Header/NavMenu.tsx
@@ -1,12 +1,22 @@
 import {
+  Avatar,
   Box,
   Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
   IconButton,
+  Link,
   Menu,
   MenuItem,
+  Modal,
+  TextField,
   Typography,
 } from '@mui/material'
-import { Menu as MenuIcon } from '@mui/icons-material'
+import {
+  Menu as MenuIcon,
+  LockOutlined as LockOutlinedIcon,
+} from '@mui/icons-material'
 import { useState } from 'react'
 
 type MenuProps = {
@@ -16,6 +26,10 @@ type MenuProps = {
 const pages = ['Войти', 'Зарегистрироваться']
 
 const NavMenu = ({ burger }: MenuProps) => {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -25,6 +39,21 @@ const NavMenu = ({ burger }: MenuProps) => {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null)
   }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    const res = await fetch('http://localhost:3002/api/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    const userData = res.json()
+    console.log(userData)
+  }
+
   return burger ? (
     <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
       <IconButton
@@ -69,14 +98,90 @@ const NavMenu = ({ burger }: MenuProps) => {
         justifyContent: 'end',
         mr: 2,
       }}>
-      {pages.map((page) => (
-        <Button
-          key={page}
-          onClick={handleCloseNavMenu}
-          sx={{ my: 2, color: 'white', display: 'block' }}>
-          {page}
-        </Button>
-      ))}
+      <Button
+        onClick={handleOpen}
+        sx={{ my: 2, color: 'white', display: 'block' }}>
+        Войти
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+          }}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Войти
+          </Typography>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+            />
+            <FormControlLabel
+              control={<Checkbox value='remember' color='primary' />}
+              label='Remember me'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}>
+              Войти
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href='#' variant='body2'>
+                  Забыли пароль?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href='/signup' variant='body2'>
+                  {'Не зарегистрированы? Создать аккаунт'}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Modal>
+      <Button
+        onClick={handleCloseNavMenu}
+        sx={{ my: 2, color: 'white', display: 'block' }}>
+        Регистрация
+      </Button>
     </Box>
   )
 }
--- client/src/components/pages/Login/Login.tsx
+++ client/src/components/pages/Login/Login.tsx
@@ -19,6 +19,18 @@ import { ThemeProvider } from '@mui/material/styles'
 import { mainTheme } from '../../themes/mainTheme'
 import { useState } from 'react'
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
+
 export default function Login() {
   // handle form submit. TODO: replace console.log with actual query to server
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -33,7 +45,6 @@ export default function Login() {
   // modal state. TODO: transfer this to RTK actions and navBar
 
   const [open, setOpen] = useState(false)
-  const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
   return (
@@ -43,72 +54,74 @@ export default function Login() {
         onClose={handleClose}
         aria-labelledby='modal-modal-title'
         aria-describedby='modal-modal-description'>
-        <Container component='main' maxWidth='xs'>
-          <CssBaseline />
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+          }}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Войти
+          </Typography>
           <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}>
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component='h1' variant='h5'>
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+            />
+            <FormControlLabel
+              control={<Checkbox value='remember' color='primary' />}
+              label='Remember me'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}>
               Войти
-            </Typography>
-            <Box
-              component='form'
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}>
-              <TextField
-                margin='normal'
-                required
-                fullWidth
-                id='email'
-                label='Email Address'
-                name='email'
-                autoComplete='email'
-                autoFocus
-              />
-              <TextField
-                margin='normal'
-                required
-                fullWidth
-                name='password'
-                label='Password'
-                type='password'
-                id='password'
-                autoComplete='current-password'
-              />
-              <FormControlLabel
-                control={<Checkbox value='remember' color='primary' />}
-                label='Remember me'
-              />
-              <Button
-                type='submit'
-                fullWidth
-                variant='contained'
-                sx={{ mt: 3, mb: 2 }}>
-                Войти
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href='#' variant='body2'>
-                    Забыли пароль?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href='/signup' variant='body2'>
-                    {'Не зарегистрированы? Создать аккаунт'}
-                  </Link>
-                </Grid>
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href='#' variant='body2'>
+                  Забыли пароль?
+                </Link>
               </Grid>
-            </Box>
+              <Grid item>
+                <Link href='/signup' variant='body2'>
+                  {'Не зарегистрированы? Создать аккаунт'}
+                </Link>
+              </Grid>
+            </Grid>
           </Box>
-        </Container>
+        </Box>
       </Modal>
     </ThemeProvider>
   )
