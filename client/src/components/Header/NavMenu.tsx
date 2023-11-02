e8395e3b56e227e33194e66c97f97104956d1789
imagine2509 <imagine2509@gmail.com>
2023-11-02 23:04:09 +0300

    reg modal

--- client/src/components/Header/NavMenu.tsx
+++ client/src/components/Header/NavMenu.tsx
@@ -1,34 +1,30 @@
 import {
-  Avatar,
   Box,
   Button,
-  Checkbox,
-  FormControlLabel,
-  Grid,
   IconButton,
-  Link,
   Menu,
   MenuItem,
   Modal,
-  TextField,
   Typography,
 } from '@mui/material'
-import {
-  Menu as MenuIcon,
-  LockOutlined as LockOutlinedIcon,
-} from '@mui/icons-material'
+import { Menu as MenuIcon } from '@mui/icons-material'
+
 import { useState } from 'react'
+import Login from '../pages/Login/Login'
+import Register from '../pages/Register/Register'
 
 type MenuProps = {
   burger: boolean
 }
 
-const pages = ['Войти', 'Зарегистрироваться']
-
 const NavMenu = ({ burger }: MenuProps) => {
-  const [open, setOpen] = useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+  const [loginOpen, setLoginOpen] = useState(false)
+  const [regOpen, setRegOpen] = useState(false)
+
+  const handleLoginOpen = () => setLoginOpen(true)
+  const handleLoginClose = () => setLoginOpen(false)
+  const handleRegOpen = () => setRegOpen(true)
+  const handleRegClose = () => setRegOpen(false)
 
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
 
@@ -38,20 +34,6 @@ const NavMenu = ({ burger }: MenuProps) => {
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null)
-  }
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
-    const res = await fetch('http://localhost:3002/api/user/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-    const userData = res.json()
-    console.log(userData)
   }
 
   return burger ? (
@@ -83,11 +65,30 @@ const NavMenu = ({ burger }: MenuProps) => {
         sx={{
           display: { xs: 'block', md: 'none' },
         }}>
-        {pages.map((page) => (
-          <MenuItem key={page} onClick={handleCloseNavMenu}>
-            <Typography textAlign='center'>{page}</Typography>
-          </MenuItem>
-        ))}
+        <MenuItem onClick={handleCloseNavMenu}>
+          <Typography onClick={handleLoginOpen} textAlign='center'>
+            Войти
+          </Typography>
+        </MenuItem>
+        <Modal
+          open={loginOpen}
+          onClose={handleLoginClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'>
+          <Login />
+        </Modal>
+        <MenuItem onClick={handleCloseNavMenu}>
+          <Typography onClick={handleRegOpen} textAlign='center'>
+            Регистрация
+          </Typography>
+        </MenuItem>
+        <Modal
+          open={regOpen}
+          onClose={handleRegClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'>
+          <Register />
+        </Modal>
       </Menu>
     </Box>
   ) : (
@@ -99,89 +100,29 @@ const NavMenu = ({ burger }: MenuProps) => {
         mr: 2,
       }}>
       <Button
-        onClick={handleOpen}
+        onClick={handleLoginOpen}
         sx={{ my: 2, color: 'white', display: 'block' }}>
         Войти
       </Button>
       <Modal
-        open={open}
-        onClose={handleClose}
+        open={loginOpen}
+        onClose={handleLoginClose}
         aria-labelledby='modal-modal-title'
         aria-describedby='modal-modal-description'>
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: 400,
-            bgcolor: 'background.paper',
-            border: '2px solid #000',
-            boxShadow: 24,
-            p: 4,
-          }}>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            Войти
-          </Typography>
-          <Box
-            component='form'
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}>
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              id='email'
-              label='Email Address'
-              name='email'
-              autoComplete='email'
-              autoFocus
-            />
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              label='Password'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-            />
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
-            />
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}>
-              Войти
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='#' variant='body2'>
-                  Забыли пароль?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='/signup' variant='body2'>
-                  {'Не зарегистрированы? Создать аккаунт'}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
+        <Login />
       </Modal>
       <Button
         onClick={handleCloseNavMenu}
         sx={{ my: 2, color: 'white', display: 'block' }}>
         Регистрация
       </Button>
+      <Modal
+        open={regOpen}
+        onClose={handleRegClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'>
+        <Register />
+      </Modal>
     </Box>
   )
 }
