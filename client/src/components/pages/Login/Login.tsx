e1d206b091ad4516f7352d2dc45ae7f0409fa72d
imagine2509 <imagine2509@gmail.com>
2023-11-02 23:41:22 +0300

    fixed modals in desktop

--- client/src/components/pages/Login/Login.tsx
+++ client/src/components/pages/Login/Login.tsx
@@ -44,85 +44,70 @@ export default function Login() {
 
   // modal state. TODO: transfer this to RTK actions and navBar
 
-  const [open, setOpen] = useState(false)
-  const handleClose = () => setOpen(false)
-
   return (
-    <ThemeProvider theme={mainTheme}>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'>
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: 400,
-            bgcolor: 'background.paper',
-            border: '2px solid #000',
-            boxShadow: 24,
-            p: 4,
-          }}>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            Войти
-          </Typography>
-          <Box
-            component='form'
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}>
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              id='email'
-              label='Email Address'
-              name='email'
-              autoComplete='email'
-              autoFocus
-            />
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              label='Password'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-            />
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
-            />
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}>
-              Войти
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='#' variant='body2'>
-                  Забыли пароль?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='/signup' variant='body2'>
-                  {'Не зарегистрированы? Создать аккаунт'}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Modal>
-    </ThemeProvider>
+    <Box
+      sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      }}>
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component='h1' variant='h5'>
+        Войти
+      </Typography>
+      <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          id='email'
+          label='Email Address'
+          name='email'
+          autoComplete='email'
+          autoFocus
+        />
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          name='password'
+          label='Password'
+          type='password'
+          id='password'
+          autoComplete='current-password'
+        />
+        <FormControlLabel
+          control={<Checkbox value='remember' color='primary' />}
+          label='Remember me'
+        />
+        <Button
+          type='submit'
+          fullWidth
+          variant='contained'
+          sx={{ mt: 3, mb: 2 }}>
+          Войти
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link href='#' variant='body2'>
+              Забыли пароль?
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href='/signup' variant='body2'>
+              {'Не зарегистрированы? Создать аккаунт'}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   )
 }
--- client/src/components/pages/Register/Register.tsx
+++ client/src/components/pages/Register/Register.tsx
@@ -13,12 +13,8 @@ import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import { ThemeProvider } from '@mui/material/styles'
 import { mainTheme } from '../../themes/mainTheme'
-import { useState } from 'react'
-import { User } from '../../../models/User'
 
-export default function SignUp() {
-  const [user, setUser] = useState<User | null>(null) // refactor to context
-
+export default function Register() {
   const handleSubmit = async (
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
@@ -33,80 +29,67 @@ export default function SignUp() {
     })
     const userData = await res.json()
     console.log(userData)
-
-    setUser(userData)
   }
 
   return (
-    <ThemeProvider theme={mainTheme}>
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            Sign up
-          </Typography>
-          <Box
-            component='form'
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id='email'
-                  label='Email Address'
-                  name='email'
-                  autoComplete='email'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name='password'
-                  label='Password'
-                  type='password'
-                  id='password'
-                  autoComplete='new-password'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value='allowExtraEmails' color='primary' />
-                  }
-                  label='I want to receive inspiration, marketing promotions and updates via email.'
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type='submit'
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component='h1' variant='h5'>
+        Зарегистрироваться
+      </Typography>
+      <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              required
               fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}>
-              Sign Up
-            </Button>
-            <Grid container justifyContent='flex-end'>
-              <Grid item>
-                <Link href='/signin' variant='body2'>
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='new-password'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={<Checkbox value='allowExtraEmails' color='primary' />}
+              label='I want to receive inspiration, marketing promotions and updates via email.'
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type='submit'
+          fullWidth
+          variant='contained'
+          sx={{ mt: 3, mb: 2 }}>
+          Зарегистрироваться
+        </Button>
+        <Grid container justifyContent='flex-end'>
+          <Grid item>
+            <Link href='/signin' variant='body2'>
+              Уже есть аккаунт? Войти
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   )
 }
