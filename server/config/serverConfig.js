847e89f6e687d7db4de0b66209198237deffb850
imagine2509 <imagine2509@gmail.com>
2023-11-06 22:56:22 +0300

    auth fix

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const cors = require('cors');
-const sessionConfig = require('./sessionConfig');
-const { cookiesCleaner, resLocals, getUser } = require('../middleware/auth');
-const ssr = require('../middleware/ssr');
+const express = require('express')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const cors = require('cors')
+const sessionConfig = require('./sessionConfig')
+const { cookiesCleaner, resLocals, getUser } = require('../middleware/auth')
+const ssr = require('../middleware/ssr')
 
 // главная конфигурация приложения
 const serverConfig = (app) => {
-	// использование middleware
-	app.use(morgan('dev'));
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
-	app.use(cors());
-	app.use(express.static('public'));
-	app.use(cookieParser());
-	app.use(session(sessionConfig));
-	app.use(cookiesCleaner);
-	app.use(resLocals);
-	app.use(getUser);
-	app.use(ssr);
-};
+  // использование middleware
+  app.use(morgan('dev'))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(cors())
+  app.use(express.static('public'))
+  app.use(cookieParser())
+  app.use(session(sessionConfig))
+  app.use(cookiesCleaner)
+  app.use(resLocals)
+  app.use(getUser)
+  app.use(ssr)
+}
 
-module.exports = serverConfig;
+module.exports = serverConfig
