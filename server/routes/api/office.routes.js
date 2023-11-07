522061fda152119a68788c98859018dcab9987e0
sedovgm <sedovgm@gmail.com>
2023-11-07 17:39:24 +0300

    office routes room routes office seed postman check

--- server/routes/api/office.routes.js
+++ server/routes/api/office.routes.js
@@ -3,23 +3,35 @@ const { Office } = require('../../db/models');
 
 router.route('/offices').get((req, res) => {
 	Office.findAll({ raw: true })
-		.then((allUsers) => res.json(allUsers))
+		.then((allOffices) => res.json(allOffices))
 		.catch((error) => res.status(500).json({ error: error.message }));
 });
 
 router
 	.route('/office/:id')
 	.get((req, res) => {
-		const { id } = req.params.id;
+		const { id } = req.params;
 		Office.findOne({ where: { id } })
-			.then((user) => res.json(user))
+			.then((office) => res.json(office))
 			.catch((error) => res.status(500).json({ error: error.message }));
 	})
 	.delete((req, res) => {
 		const { id } = req.params;
-		User.destroy({ where: { id } })
+		Office.destroy({ where: { id } })
 			.then((deletedOffice) =>
 				deletedOffice ? res.json(id) : res.status(404).json(deletedOffice)
 			)
 			.catch((error) => res.status(500).json({ error: error.message }));
 	});
+
+router.route('/office')
+	.post((req, res) => {
+		const { name , address } = req.body;
+		Office.create({ name , address})
+			.then((newOffice) =>
+				newOffice ? res.status(200).json({id:newOffice.id,name:newOffice.name,address:newOffice.address}) : res.status(409).json({message:`failed to create new office with name=${name} and address=${address}`}) //409 conflict
+			)
+			.catch((error) => res.status(500).json({ error: error.message }));
+	})
+
+module.exports = router;
