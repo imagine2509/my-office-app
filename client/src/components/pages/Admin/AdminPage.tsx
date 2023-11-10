967a7231cda95d80a9bbc2c0ce98b30ebe8e6d0b
imagine2509 <imagine2509@gmail.com>
2023-11-10 21:08:36 +0300

    structure fix

--- client/src/components/pages/Admin/AdminPage.tsx
+++ client/src/components/pages/Admin/AdminPage.tsx
@@ -1,9 +1,9 @@
-import { Button, Grid, Typography } from "@mui/material"
-import { useState } from "react"
-import { officeAPI } from "../../../hooks/officeService"
-import { roomAPI } from "../../../hooks/roomService"
-import OfficeMenuItem from "./OfficeMenuItem"
-import RoomCard from "./RoomCard/RoomCard"
+import { Button, Grid, Typography } from '@mui/material'
+import { useState } from 'react'
+import { officeAPI } from '../../../hooks/officeService'
+import { roomAPI } from '../../../hooks/roomService'
+import OfficeMenuItem from './OfficeMenuItem'
+import RoomCard from './RoomCard/RoomCard'
 
 import styles from './admin.style.module.scss'
 
@@ -12,14 +12,13 @@ const AdminPage = () => {
     const offices = allOffices.data ?? []
     const allRooms = roomAPI.useGetAllRoomsQuery(null)
     const rooms = allRooms.data ?? []
-    const userOfficeId = localStorage.getItem('officeId') != null ? Number(localStorage.getItem('officeId')) : 1
-
-
+    const userOfficeId =
+        localStorage.getItem('officeId') != null
+            ? Number(localStorage.getItem('officeId'))
+            : 1
 
     const [expanded, setExpanded] = useState<number | false>(false)
-    const [selectedOffice, setSelectedOffice] = useState<number>(
-        userOfficeId
-    )
+    const [selectedOffice, setSelectedOffice] = useState<number>(userOfficeId)
 
     const handleChange =
         (office: number) => (event: React.SyntheticEvent, isExpanded: boolean) => {
@@ -37,12 +36,14 @@ const AdminPage = () => {
                 <Typography variant='h5' gutterBottom p={1}>
                     Выбор офиса:
                 </Typography>
-                {
-                    offices.map((office) => (
-                        <OfficeMenuItem {...{ ...office, expanded, handleChange, selectedOffice }} />
-                    ))
-                }
-                <Button type="button" variant="outlined" className={styles.editButton}>Редактировать</Button>
+                {offices.map((office) => (
+                    <OfficeMenuItem
+                        {...{ ...office, expanded, handleChange, selectedOffice }}
+                    />
+                ))}
+                <Button type='button' variant='outlined' className={styles.editButton}>
+                    Редактировать
+                </Button>
             </Grid>
             <Grid item>
                 <Grid container spacing={2}>
@@ -50,8 +51,12 @@ const AdminPage = () => {
                         (room) => room.officeId === selectedOffice && <RoomCard {...room} />
                     )}
                 </Grid>
+                {expanded ? <Button type='button' variant='outlined' className={styles.editButton}>
+                    Редактировать
+                </Button> : <></>}
             </Grid>
         </Grid>
+
     )
 }
 
