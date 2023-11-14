9a9009ec03df83a711151441ba6975c9100fac32
sedovgm <sedovgm@gmail.com>
2023-11-14 18:52:07 +0300

    rooms edit , new .delete route , badge

--- client/src/components/pages/RoomsPage/RoomCard/RoomCard.tsx
+++ client/src/components/pages/RoomsPage/RoomCard/RoomCard.tsx
@@ -1,13 +1,14 @@
+/* eslint-disable no-empty-pattern */
 import {
-	Box,
-	Button,
-	Card,
-	CardActions,
-	CardContent,
-	CardMedia,
-	Grid,
-	Tooltip,
-	Typography,
+  Badge,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Tooltip,
+  Typography,
 } from '@mui/material';
 import DuoIcon from '@mui/icons-material/Duo';
 import VideocamOffIcon from '@mui/icons-material/VideocamOff';
@@ -17,79 +18,85 @@ import { roomAPI } from '../../../../hooks/roomService';
 import { useNavigate } from 'react-router-dom';
 
 interface Props {
-	officeId: number;
-	name: string;
-	amount: number;
-	video: boolean;
-	description: string;
-	photo: string;
-	id: number;
+  officeId: number;
+  name: string;
+  amount: number;
+  video: boolean;
+  description: string;
+  photo: string;
+  id: number;
 }
 
 function RoomCard(props: Props) {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const allRooms = roomAPI.useGetAllRoomsQuery(null);
+  const allRooms = roomAPI.useGetAllRoomsQuery(null);
 
-	const { photo, name, description, video, id } = props;
+  const { photo, name, description, video, id, amount } = props;
 
-	const [changeRoom, {}] = roomAPI.useChangeRoomMutation();
-	const handleChangeRoom = async () => {
-		const description = prompt() || '';
-		await changeRoom({ ...props, description });
-		allRooms.refetch();
-	};
-	const [deleteRoom, {}] = roomAPI.useDeleteRoomMutation();
-	const handleDeleteRoom = async (id: number) => {
-		await deleteRoom(id);
-		allRooms.refetch();
-	};
+  const [changeRoom, {}] = roomAPI.useChangeRoomMutation();
+  const handleChangeRoom = async () => {
+    const description = prompt() || '';
+    await changeRoom({ ...props, description });
+    allRooms.refetch();
+  };
+  const [deleteRoom, {}] = roomAPI.useDeleteRoomMutation();
+  const handleDeleteRoom = async (id: number) => {
+    await deleteRoom(id);
+    allRooms.refetch();
+  };
 
-	return (
-		<Grid item key={name}>
-			<Card className={styles.roomCard}>
-				<CardMedia
-					component="img"
-					height="140"
-					image={photo}
-					alt={description}
-				/>
-				<CardContent>
-					<Typography variant="h5">{name}</Typography>
-					<Typography variant="body2" color="text.secondary">
-						{description}
-					</Typography>
-				</CardContent>
-				<CardActions>
-					<Grid
-						container
-						direction="row"
-						justifyContent="space-between"
-						alignItems="center"
-					>
-						{video ? (
-							<Tooltip title="Видеосвязь есть" placement="top-end">
-								<DuoIcon />
-							</Tooltip>
-						) : (
-							<Tooltip title="Видеосвязи нет" placement="top-end">
-								<VideocamOffIcon />
-							</Tooltip>
-						)}
-						<Button size="small" onClick={() => navigate(`/rooms/${id}`)}>
-							Подробнее
-						</Button>
-						<Button size="small" onClick={() => handleChangeRoom()}>
-							Изменить
-						</Button>
-						<Button size="small" onClick={() => handleDeleteRoom(id)}>
-							Удалить
-						</Button>
-					</Grid>
-				</CardActions>
-			</Card>
-		</Grid>
-	);
+  return (
+    <Grid item key={name}>
+      <Card className={styles.roomCard}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={photo}
+          alt={description}
+        />
+        <CardContent>
+          <Badge
+            badgeContent={amount}
+            color="primary"
+            anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+          >
+            <Typography variant="h5">{name}</Typography>
+          </Badge>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            {video ? (
+              <Tooltip title="Видеосвязь есть" placement="top-end">
+                <DuoIcon />
+              </Tooltip>
+            ) : (
+              <Tooltip title="Видеосвязи нет" placement="top-end">
+                <VideocamOffIcon />
+              </Tooltip>
+            )}
+            <Button size="small" onClick={() => navigate(`/rooms/${id}`)}>
+              Подробнее
+            </Button>
+            <Button size="small" onClick={() => handleChangeRoom()}>
+              Изменить
+            </Button>
+            <Button size="small" onClick={() => handleDeleteRoom(id)}>
+              Удалить
+            </Button>
+          </Grid>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
 }
 
 export default RoomCard;
--- server/routes/api/room.routes.js
+++ server/routes/api/room.routes.js
@@ -1,66 +1,74 @@
-/* eslint-disable max-len */
 /* eslint-disable linebreak-style */
+/* eslint-disable max-len */
 const router = require('express').Router();
-const { Room } = require('../../db/models');
+const { Room, UserRoom } = require('../../db/models');
 
 router
-	.route('/:id')
-	.get((req, res) => {
-		const { id } = req.params;
-		Room.findOne({ where: { id } })
-			.then((room) => res.json(room))
-			.catch((error) => res.status(500).json({ error: error.message }));
-	})
-	.delete((req, res) => {
-		const { id } = req.params;
-		Room.destroy({ where: { id } })
-			.then((deletedRoom) =>
-				deletedRoom ? res.json(id) : res.status(404).json(deletedRoom)
-			)
-			.catch((error) => res.status(500).json({ error: error.message }));
-	})
-	.put((req, res) => {
-		const { id } = req.params;
-		Room.update(req.body, { where: { id } })
-			.then((updatedRoom) =>
-				updatedRoom ? res.json(updatedRoom) : res.status(404).json(updatedRoom)
-			)
-			.catch((error) => res.status(500).json({ error: error.message }));
-	});
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    Room.findOne({ where: { id } })
+      .then((room) => res.json(room))
+      .catch((error) => res.status(500).json({ error: error.message }));
+  })
+  .delete(async (req, res) => {
+    try {
+      const { id } = req.params;
+      await UserRoom.destroy(
+        {
+          where: {
+            roomId: id,
+          },
+        },
+      );
+      const room = await Room.findOne({ where: { id } });
+      room.destroy();
+      res.json(id);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  })
+  .put((req, res) => {
+    const { id } = req.params;
+    Room.update(req.body, { where: { id } })
+      .then((updatedRoom) => (updatedRoom ? res.json(updatedRoom) : res.status(404).json(updatedRoom)))
+      .catch((error) => res.status(500).json({ error: error.message }));
+  });
 
 router
-	.route('/')
-	.get(async (req, res) => {
-		try {
-			const result = await Room.findAll();
-			res.json(result);
-		} catch (error) {
-			console.log(error.message);
-		}
-	})
-	.post((req, res) => {
-		const { name, amount, officeId, video, description, photo } = req.body;
-		Room.create({
-			name,
-			amount,
-			officeId,
-			video,
-			description,
-			photo,
-		})
-			.then(
-				(newRoom) =>
-					newRoom
-						? res.status(200).json({
-								id: newRoom.id,
-								name: newRoom.name,
-								description: newRoom.description,
-						  })
-						: res.status(409).json({
-								message: `failed to create new room with name=${name}`,
-						  }) // 409 conflict
-			)
-			.catch((error) => res.status(500).json({ error: error.message }));
-	});
+  .route('/')
+  .get(async (req, res) => {
+    try {
+      const result = await Room.findAll();
+      res.json(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+  })
+  .post((req, res) => {
+    const {
+      name, amount, officeId, video, description, photo,
+    } = req.body;
+    Room.create({
+      name,
+      amount,
+      officeId,
+      video,
+      description,
+      photo,
+    })
+      .then(
+        (newRoom) => (newRoom
+          ? res.status(200).json({
+            id: newRoom.id,
+            name: newRoom.name,
+            description: newRoom.description,
+          })
+          : res.status(409).json({
+            message: `failed to create new room with name=${name}`,
+          })), // 409 conflict
+      )
+      .catch((error) => res.status(500).json({ error: error.message }));
+  });
 
 module.exports = router;
