5bd589e23ab311e9112744dd0c6847421b418984
VInnokentiy <v.innokentiy@vk.com>
2023-11-17 20:04:09 +0300

    fix calendar

--- client/src/components/pages/RoomsPage/RoomCard/TestRoomCard.tsx
+++ client/src/components/pages/RoomsPage/RoomCard/TestRoomCard.tsx
@@ -1,129 +1,179 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
-import { TimePicker } from '@mui/x-date-pickers/TimePicker'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { format } from 'date-fns'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import { Box, Button } from '@mui/material'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { format } from 'date-fns';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Box, Button } from '@mui/material';
+
+type AcceptedData = {
+	$d: Date;
+};
 
 type Booking = {
-  id: number | string
-  startTime: Date
-  endTime: Date
-}
+	id: number | string;
+	startTime: Date;
+	endTime: Date;
+};
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
 function TestRoomCard() {
-  const [currentDate, setCurrentDate] = useState(new Date())
-  const [startTime, setStartTime] = useState()
-  const [endTime, setEndTime] = useState()
-  const { id } = useParams()
-  const userId = localStorage.getItem('id')
+	const [currentDate, setCurrentDate] = useState(new Date());
+	const [startTime, setStartTime] = useState<Date>();
+	const [endTime, setEndTime] = useState<Date>();
+	const { id } = useParams();
+	const userId = localStorage.getItem('id');
+	const transformDate = (notToday: Date): Date | undefined => {
+		if (notToday) {
+			const resStart = new Date(
+				notToday
+					.toString()
+					.split(' ')
+					.map((el, i) => {
+						if (i === 2) {
+							el = currentDate.toString().split(' ')[2];
+							return el;
+						}
+						return el;
+					})
+					.join(' ')
+			);
+			return resStart;
+		}
+	};
 
-  const handleSubmit = async (
-    event: React.FormEvent<HTMLFormElement>
-  ): Promise<void> => {
-    event.preventDefault()
+	const handleSubmit = async (
+		event: React.FormEvent<HTMLFormElement>
+	): Promise<void> => {
+		event.preventDefault();
+		// console.log(currentDate);
 
-    try {
-      const data = { startTime, endTime, id, userId }
-      console.log(data)
-      const res = await fetch(`http://localhost:3002/api/userroom/${id}`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-      console.log(res)
-    } catch (err) {
-      console.log(err)
-    }
-  }
+		try {
+			const data = { startTime, endTime, id, userId };
+			console.log(data);
 
-  const [booking, setBookings] = useState<Booking[]>([
-    {
-      startTime: new Date(),
-      endTime: new Date(),
-      id: 0,
-    },
-  ])
+			// if (startTime) {
+			// 	const resStart = new Date(
+			// 		startTime
+			// 			.toString()
+			// 			.split(' ')
+			// 			.map((el, i) => {
+			// 				if (i === 2) {
+			// 					el = currentDate.toString().split(' ')[2];
+			// 					return el;
+			// 				}
+			// 				return el;
+			// 			})
+			// 			.join(' ')
+			// 	);
+			// }
+			const res = await fetch(`http://localhost:3002/api/userroom/${id}`, {
+				method: 'POST',
+				credentials: 'include',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			});
+			console.log(res);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  useEffect(() => {
-    const fetchBookings = async () => {
-      const allBookings = await fetch(
-        `http://localhost:3002/api/userroom/${id}`
-      )
-      const res = await allBookings.json()
-      console.log(res)
+	const [booking, setBookings] = useState<Booking[]>([
+		{
+			startTime: new Date(),
+			endTime: new Date(),
+			id: 0,
+		},
+	]);
 
-      const result = res.map((el: Booking) => {
-        el.endTime = new Date(el.endTime)
-        el.startTime = new Date(el.startTime)
-        return el
-      })
-      setBookings(result)
-    }
-    fetchBookings()
-  }, [])
-  return (
-    <div>
-      <div>Все брони для комнаты с ID:{id}</div>
-      {booking.map((date, index) => {
-        return (
-          <div key={index}>
-            <div>
-              <span>
-                Дата брони: {format(new Date(date.startTime), 'MM-dd')}
-              </span>
-              <span>
-                Время брони с {format(new Date(date.startTime), 'HH-mm')}
-              </span>
-              <span> до {format(new Date(date.endTime), 'HH-mm')}</span>
-            </div>
-          </div>
-        )
-      })}
-      <div>
-        <Calendar
-          date={currentDate}
-          onNavigate={setCurrentDate}
-          localizer={localizer}
-          defaultDate={new Date()}
-          events={booking}
-          startAccessor='startTime'
-          endAccessor='endTime'
-          style={{ height: 350, width: 700 }}
-        />
-      </div>
-      <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DemoContainer components={['TimePicker', 'TimePicker']}>
-            <TimePicker
-              ampm={false}
-              label='Start Time'
-              onAccept={(value) => setStartTime(value.$d)}
-            />
-            <TimePicker
-              ampm={false}
-              label='End Time'
-              onAccept={(value) => setEndTime(value.$d)}
-            />
-          </DemoContainer>
-        </LocalizationProvider>
-        <Button type='submit' variant='contained' sx={{ mt: 3, mb: 2 }}>
-          Забронировать
-        </Button>
-      </Box>
-    </div>
-  )
+	useEffect(() => {
+		const fetchBookings = async () => {
+			const allBookings = await fetch(
+				`http://localhost:3002/api/userroom/${id}`
+			);
+			const res = await allBookings.json();
+			console.log(res);
+
+			const result = res.map((el: Booking) => {
+				el.endTime = new Date(el.endTime);
+				el.startTime = new Date(el.startTime);
+				return el;
+			});
+			setBookings(result);
+		};
+		fetchBookings();
+	}, []);
+	return (
+		<div>
+			<div>Все брони для комнаты с ID:{id}</div>
+			{booking.map((date, index) => {
+				return (
+					<div key={index}>
+						<div>
+							<span>
+								Дата брони: {format(new Date(date.startTime), 'MM-dd-yyyy')}
+							</span>
+							<span>
+								Время брони с {format(new Date(date.startTime), 'HH-mm')}
+							</span>
+							<span> до {format(new Date(date.endTime), 'HH-mm')}</span>
+						</div>
+					</div>
+				);
+			})}
+			<div>
+				<Calendar
+					date={currentDate}
+					onNavigate={setCurrentDate}
+					localizer={localizer}
+					defaultDate={new Date()}
+					events={booking}
+					startAccessor="startTime"
+					endAccessor="endTime"
+					style={{ height: 350, width: 700 }}
+					messages={{
+						next: 'Следующий',
+						previous: 'Предыдущий',
+						today: 'Сегодня',
+						month: 'Месяц',
+						week: 'Неделя',
+						day: 'День',
+						agenda: 'Текущий день',
+					}}
+				/>
+			</div>
+			<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+				<LocalizationProvider dateAdapter={AdapterDayjs}>
+					<DemoContainer components={['TimePicker', 'TimePicker']}>
+						<TimePicker
+							ampm={false}
+							label="Старт брони"
+							onAccept={(value) => setStartTime(transformDate(value.$d))}
+							onChange={(value) => setStartTime(transformDate(value.$d))}
+						/>
+						<TimePicker
+							ampm={false}
+							label="Конец брони"
+							onAccept={(value) => setEndTime(transformDate(value.$d))}
+							onChange={(value) => setEndTime(transformDate(value.$d))}
+						/>
+					</DemoContainer>
+				</LocalizationProvider>
+				<Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+					Забронировать
+				</Button>
+			</Box>
+		</div>
+	);
 }
 
-export default TestRoomCard
+export default TestRoomCard;
--- server/routes/api/userRoom.routes.js
+++ server/routes/api/userRoom.routes.js
@@ -1,43 +1,53 @@
-const router = require('express').Router()
-const { UserRoom } = require('../../db/models')
+const router = require('express').Router();
+const { UserRoom } = require('../../db/models');
 
 router.route('/').get((req, res) => {
-  UserRoom.findAll({ raw: true })
-    .then((allUserRoom) => res.json(allUserRoom))
-    .catch((error) => res.status(500).json({ error: error.message }))
-})
+	UserRoom.findAll({ raw: true })
+		.then((allUserRoom) => res.json(allUserRoom))
+		.catch((error) => res.status(500).json({ error: error.message }));
+});
 
 router
-  .route('/:roomId')
-  .post((req, res) => {
-    try {
-      const { startTime, endTime } = req.body
-      const { roomId } = req.params
-      // const { userId } = res.localStorage.getItem;
-      UserRoom.create({ startTime, endTime, roomId })
-        .then(
-          (newUserRoom) =>
-            newUserRoom
-              ? res.status(200).json({
-                  id: newUserRoom.id,
-                  startTime: newUserRoom.startTime,
-                  endTime: newUserRoom.endTime,
-                })
-              : res.status(409).json({
-                  message: `failed to book room`,
-                }) // 409 conflict
-        )
-        .catch((error) => res.status(500).json({ error: error.message }))
-    } catch (error) {
-      console.log(error.message)
-    }
-  })
-  .get((req, res) => {
-    const { roomId } = req.params
-    console.log(roomId)
-    UserRoom.findAll({ where: { roomId }, raw: true })
-      .then((allUserRoom) => res.json(allUserRoom))
-      .catch((error) => res.status(500).json({ error: error.message }))
-  })
+	.route('/:roomId')
+	.post((req, res) => {
+		try {
+			const { startTime, endTime } = req.body;
+			const { roomId } = req.params;
+			// const { userId } = res.localStorage.getItem;
+			UserRoom.create({ startTime, endTime, roomId })
+				.then(
+					(newUserRoom) =>
+						newUserRoom
+							? res.status(200).json({
+									id: newUserRoom.id,
+									startTime: newUserRoom.startTime,
+									endTime: newUserRoom.endTime,
+							  })
+							: res.status(409).json({
+									message: `failed to book room`,
+							  }) // 409 conflict
+				)
+				.catch((error) => res.status(500).json({ error: error.message }));
+		} catch (error) {
+			console.log(error.message);
+		}
+	})
+	.get((req, res) => {
+		const { roomId } = req.params;
+		UserRoom.findAll({ where: { roomId }, raw: true })
+			.then((allUserRoom) => res.json(allUserRoom))
+			.catch((error) => res.status(500).json({ error: error.message }));
+	})
+	.delete(async (req, res) => {
+		try {
+			const { roomId } = req.params;
+			const response = await UserRoom.destroy({ where: { id: roomId } });
+			if (response) {
+				res.sendStatus(200);
+			}
+		} catch ({ message }) {
+			res.status(500).json({ message });
+		}
+	});
 
-module.exports = router
+module.exports = router;
